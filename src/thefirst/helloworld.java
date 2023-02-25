c0ee0d0a8177632a79e446ab682f25e379d27a9b
泮驰 <@Pccccc>
2023-02-25 09:00:15 +0800

    My first java program "Hello world"

---
+++ src/thefirst/helloworld.java
@@ -0,0 +1,10 @@
+package thefirst;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello world!");
+	}
+
+}
